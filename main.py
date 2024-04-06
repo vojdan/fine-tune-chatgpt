b325e4cd28f108d3f94c0a0a77b33f6d4be62556
vojdan <vojdank@gmail.com>
2024-04-05 22:52:13 -0700

    save output to file

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 import csv
 import json
 
-csv_file_path = 'tuning.csv'
+# Path to your CSV file
+csv_file_path = 'data/tuning.csv'
+# Path to the output JSONL file
+jsonl_file_path = 'data/tuning.jsonl'
 
-with open(csv_file_path, mode='r', encoding='utf-8') as csv_file:
+# Open the CSV file for reading
+with open(csv_file_path, mode='r', encoding='utf-8') as csv_file, open(jsonl_file_path, mode='w', encoding='utf-8') as jsonl_file:
+    # Create a CSV reader object
     csv_reader = csv.reader(csv_file)
     next(csv_reader)  # Skip the header row
+
     
     for row in csv_reader:
         user_content, assistant_content = row
@@ -20,4 +26,5 @@ with open(csv_file_path, mode='r', encoding='utf-8') as csv_file:
             ]
         }
         
-        print(json.dumps(message_json))
+        # Write the JSON object to the JSONL file, one object per line
+        jsonl_file.write(json.dumps(message_json) + '\n')
